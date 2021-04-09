424a521fdbbe5156cd4966ef149f2d8db863591e
Adriano Ivan <adrianoivandepaulacampos@gmail.com.br>
2021-04-09 13:24:49 -0300

    Projeto finalizado.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 "use strict";
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 30) + 1;
 let score = 20;
 let highscore = 0;
 
 document.querySelector("#score").textContent = score;
-document.querySelector("#number").textContent = secretNumber;
 
 document.querySelector("#confirm").addEventListener("click", function () {
   const guess = Number(document.querySelector("#choosen-number").value);
@@ -14,6 +13,13 @@ document.querySelector("#confirm").addEventListener("click", function () {
     document.querySelector("#situation").textContent = "There is no number !";
   } else if (guess === secretNumber) {
     document.querySelector("#situation").textContent = "Correct number !";
+
+    document.querySelector("#number").textContent = secretNumber;
+
+    document.querySelector("#user-interact").style.backgroundColor = "#382";
+
+    document.querySelector("#highscore").style.color = "red";
+    document.querySelector("#score").style.color = "red";
 
     if (score > highscore) {
       highscore = score;
@@ -31,4 +37,21 @@ document.querySelector("#confirm").addEventListener("click", function () {
       document.querySelector("#situation").textContent = "You lost the game !";
     }
   }
+});
+
+document.querySelector("#again-button").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 30) + 1;
+  document.querySelector("#choosen-number").value = "";
+
+  document.querySelector("#situation").textContent = "Start guessing...";
+
+  document.querySelector("#user-interact").style.backgroundColor = "#238";
+
+  document.querySelector("#highscore").style.color = "#777";
+  document.querySelector("#score").style.color = "#777";
+
+  document.querySelector("#number").textContent = "?";
+
+  score = 20;
+  document.querySelector("#score").textContent = score;
 });
